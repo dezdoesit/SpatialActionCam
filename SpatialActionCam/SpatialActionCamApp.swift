a81e80c93e3dfebb603bb3b09af32674304a86cc
dezdoesit <dez.blair06@gmail.com>
2024-11-09 12:53:56 -0500

    Initial Commit

---
+++ SpatialActionCam/SpatialActionCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialActionCamApp.swift
+//  SpatialActionCam
+//
+//  Created by Dezmond Blair on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialActionCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
